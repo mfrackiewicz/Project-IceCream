46f48c632a418f668ade0bd0737fba9d914b934f
mfrackiewicz <131667291+mfrackiewicz@users.noreply.github.com>
2023-09-12 15:34:17 +0000

    Deploying to gh-pages from @ mfrackiewicz/Project-IceCream@f86cb074252e00bf89ae81fabe65fbac8af25778 🚀

---
+++ index.b419cba9.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[modal-about-open]"),closeModalBtn:document.querySelector("[modal-about-close]"),modal:document.querySelector("[about-data-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.b419cba9.js.map
