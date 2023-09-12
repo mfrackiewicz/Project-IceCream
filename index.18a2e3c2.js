18af4e9b7795103aa5d77ef51cfc4e8d12c95b67
mfrackiewicz <131667291+mfrackiewicz@users.noreply.github.com>
2023-09-12 09:00:09 +0000

    Deploying to gh-pages from @ mfrackiewicz/Project-IceCream@939653b3dc87acb3f376f3f1a7645a5920c06495 🚀

---
+++ index.18a2e3c2.js
@@ -0,0 +1,2 @@
+const swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.18a2e3c2.js.map
--- index.eab6a549.js
+++
@@ -1,2 +0,0 @@
-const swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{clickable:!0,el:".swiper-pagination"}});
-//# sourceMappingURL=index.eab6a549.js.map
