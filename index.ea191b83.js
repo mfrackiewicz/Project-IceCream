5c622d575c328d36e959d9cc310ca4641a1b0d88
mfrackiewicz <131667291+mfrackiewicz@users.noreply.github.com>
2023-09-15 16:09:42 +0000

    Deploying to gh-pages from @ mfrackiewicz/Project-IceCream@5b0e99686c5e429e0683ba982b30f5cb3e204c58 🚀

---
+++ index.ea191b83.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[mobile-modal-open]"),closeModalBtn:document.querySelector("[mobile-modal-close]"),modal:document.querySelector("[mobile-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.ea191b83.js.map
